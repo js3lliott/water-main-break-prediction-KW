8729dbe3dfd38c88918b85fc4dcc977e3cb89284
Jordan Samek <jordan.samek@gmail.com>
2023-02-09 17:09:10 -0500

    Experimenting with visuals for predictions

--- st_app.py
+++ st_app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
+import leafmap.foliumap as leafmap
+
+# st.set_page_config(layout="wide")
 
 DATA_PATH = ("data/processed/cleaned_break_data.csv")
 
@@ -36,11 +39,82 @@ def load_data():
 
 data = load_data()
 
+# create a dropdown menu to select the graph to display
+st.selectbox("Select a graph", ["Scatterplot", "Heatmap"])
+
 
 st.markdown("## Scatterplot of Water Main Breaks")
+# have each of the points labeled with their attributes
+
 st.map(data)
 
 
 st.markdown("## Heatmap of Water Main Breaks")
 st.plotly_chart(px.density_mapbox(data, lat='latitude', lon='longitude', z='num_breaks', radius=10,
                                     center=dict(lat=43.4643, lon=-80.5204), zoom=10, mapbox_style="carto-positron"))
+
+
+# load in the model data
+# model_data = pd.read_csv("data/processed/model_data.csv")
+
+# load in the predictions from the baseline model notebook
+# predictions = pd.read_csv("data/processed/predictions.csv")
+
+# load in the final data
+# final_data = pd.read_csv("data/processed/final_data.csv")
+
+# # drop the rows with infinite failure rates
+# inf_values = final_data[final_data['failure_rate'] == np.inf]
+# final_data = final_data.drop(inf_values.index, axis=0)
+
+# final_data.drop(['incident_date', 'asset_year_installed'], axis=1, inplace=True)
+
+# final_X = final_data.drop('failure_rate', axis=1)
+# final_y = final_data['failure_rate']
+
+# X_train, X_test, y_train, y_test = train_test_split(final_X, final_y, test_size=0.2, random_state=42)
+# rf = RandomForestRegressor()
+# rf.fit(X_train, y_train)
+
+# y_pred = rf.predict(X_test)
+
+# lat = X_test['latitude'].values
+# lon = X_test['longitude'].values
+# break_locations = list(zip(lat, lon))
+
+# load in the test data
+test_prediction_data = pd.read_csv("data/processed/test_predict_data.csv")
+
+
+st.markdown("## Predicted Water Main Breaks")
+st.plotly_chart(px.density_mapbox(test_prediction_data, lat='latitude', lon='longitude', z='predictions', radius=10,
+                                    center=dict(lat=43.4643, lon=-80.5204), zoom=10, mapbox_style="carto-positron"))
+
+st.markdown("## Predicted Water Main Breaks by Age")
+st.plotly_chart(px.density_mapbox(test_prediction_data, lat='latitude', lon='longitude', z='predictions', radius=10,
+                                    center=dict(lat=43.4643, lon=-80.5204), zoom=10, mapbox_style="carto-positron", animation_frame=test_prediction_data['age_at_break'].sort_values(ascending=True)))
+
+
+# create a hexagon layer map for the predicted water main breaks and make things translucent
+st.markdown("## Predicted Water Main Breaks")
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=43.4643,
+        longitude=-80.5204,
+        zoom=10,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=test_prediction_data,
+            get_position='[longitude, latitude]',
+            radius=150,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+    ],
+))

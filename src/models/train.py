96d96ead15e1211bffbd8c2307d1eb50d4658a80
Jordan Samek <jordan.samek@gmail.com>
2023-02-10 23:09:11 -0500

    Fixing errors in data processing file

--- src/models/train.py
+++ src/models/train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+from src.data.extract_data import extract_data
 from src.features.process_data import process_data
 
 import sklearn
@@ -8,3 +9,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import make_pipeline
+
+
+# Collect the data
+data = extract_data()
+
+# Process the data
+data = process_data(data)
+
+# Split the data
+X = data.drop(['num_breaks'], axis=1)
